f49f79a0d0cea3855858ad8ccf88110c49fc7309
macCali <jungcali94@gmail.com>
2023-04-13 23:03:56 +0900

    day11 3/3 풀이

---
+++ src/programmers/level01/day11/_005_이진변환반환하기.java
@@ -0,0 +1,25 @@
+package programmers.level01.day11;
+
+public class _005_이진변환반환하기 {
+
+    public int[] solution(String s) {
+        int count = 0, zeroCnt = 0;
+        while (!s.equals("1")) {
+            count++;
+            int prevLength = s.length();
+            String s1 = s.replace("0", "");
+            int nowLength = s1.length();
+            zeroCnt += prevLength - nowLength;
+            s = Integer.toBinaryString(nowLength);
+        }
+
+        return new int[]{count, zeroCnt};
+    }
+
+    public static void main(String[] args) {
+        int[] solution = new _005_이진변환반환하기().solution("110010101001");
+        for (int i : solution) {
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ src/programmers/level01/day11/_006_숫자의표현.java
@@ -0,0 +1,35 @@
+package programmers.level01.day11;
+
+public class _006_숫자의표현 {
+
+    public int solution(int n) {
+        if (n < 3) return 1;
+
+        int answer = 1;
+        int limit = n / 2 == 0 ? n / 2 : n / 2 + 1;
+        int sum = 0;
+        int i = 1, j = 1;
+        while (i <= limit) {
+            sum += i++;
+            if (sum < n) {
+                continue;
+            }
+
+            if (sum > n) {
+                sum -= --i;
+                sum -= j++;
+            }
+
+            if (sum == n) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int solution = new _006_숫자의표현().solution(2);
+        System.out.println("solution = " + solution);
+    }
+}
