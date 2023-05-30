0b68e0324bbdae59e617472b8df6773eb8d7819f
macCali <jungcali94@gmail.com>
2023-05-30 21:23:17 +0900

    힙 풀이 1/3

---
+++ src/programmers/algorithm/heap/_02_디스크컨트롤러.java
@@ -0,0 +1,14 @@
+package programmers.algorithm.heap;
+
+public class _02_디스크컨트롤러 {
+
+    public static void main(String[] args) {
+        int solution = new _02_디스크컨트롤러().solution(new int[][]{{0, 3}, {1, 9}, {2, 6}});
+        System.out.println("solution = " + solution);
+    }
+
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        return answer;
+    }
+}
