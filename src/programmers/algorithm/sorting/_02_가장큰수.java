51488edff4380ec0c25d1348e52091d38ecb90c3
chanwoothecali <jungcali94@gmail.com>
2023-06-08 10:08:58 +0900

    [_02_가장큰수] 문제풀이 수정

--- src/programmers/algorithm/sorting/_02_가장큰수.java
+++ src/programmers/algorithm/sorting/_02_가장큰수.java
@@ -6,30 +6,26 @@ import java.util.Comparator;
 public class _02_가장큰수 {
 
     public static void main(String[] args) {
-        String solution = new _02_가장큰수().solution(new int[]{3, 30, 34, 5, 9});
+        String solution = new _02_가장큰수().solution(new int[]{0,0});
         System.out.println(solution);
     }
 
     public String solution(int[] numbers) {
         StringBuilder sb = new StringBuilder();
-        Arrays.stream(numbers).mapToObj(i -> String.valueOf(i))
-            .sorted(getComparator())
-            .forEach(i -> sb.append(i));
+        Arrays.stream(numbers).mapToObj(String::valueOf).sorted(getComparator()).forEach(s -> sb.append(s));
+        String answer = sb.toString();
+        if (answer.startsWith("0")) {
+            return "0";
+        }
 
-        StringBuilder test = new StringBuilder();
-        Arrays.stream(numbers).mapToObj(String::valueOf).sorted(Comparator.reverseOrder()).forEach(test::append);
-        System.out.println(test);
-
-        return sb.toString();
+        return answer;
     }
 
     private Comparator<String> getComparator() {
         return (o1, o2) -> {
-            if (o1.length() == o2.length()) {
-                return o2.compareTo(o1);
-            }
-            System.out.println(o1 + ", " + o2);
-            return o2.indexOf(0) - o1.indexOf(0);
+            String concat1 = o1.concat(o2);
+            String concat2 = o2.concat(o1);
+            return concat2.compareTo(concat1);
         };
     }
 }
